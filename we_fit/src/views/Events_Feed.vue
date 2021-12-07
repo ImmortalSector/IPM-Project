ff0516a6b8c4067a6bbd77c95d770f41de6af166
francisrdmendes <72984732+francisrdmendes@users.noreply.github.com>
2021-12-07 23:33:20 +0000

    Events_Feed Calendar update

--- we_fit/src/views/Events_Feed.vue
+++ we_fit/src/views/Events_Feed.vue
@@ -8,14 +8,18 @@
           <select id="typeInput" v-model="this.type"  class="form-select" aria-label="Default select example" >
             <option :value="event_type.name" :key="event_type.name" v-for="(event_type) in this.event_types">{{event_type.name}}</option>
           </select>
+         
+          <label class="form-label" for="startDate" style="margin-right:1.2rem; font-size: x-large">Start Date </label>
+          <input @change="onChangeStart()" class="form-control" type="date" id="startDate" name="startDate" min="2021-07-01" placeholder="Enter Date..." v-model="startDate">
 
-          <Calendar style="margin-left:2.5rem;"/>
+          <label class="form-label" for="endDate" style="margin-right:1.2rem; font-size: x-large">End Date</label>
+          <input @change="onChangeEnd()" class="form-control" type="date" id="endDate" name="endDate" min="2021-07-01" placeholder="Enter Date..." v-model="endDate">
 
+          <label class="form-label" for="myEvents" style="margin-right:1.2rem; font-size: x-large">My Events</label>
           <input style="margin-left:2.5rem;" type="checkbox" id="myEvents" value="My Events" v-model="checkedMyEvents">
-          <label style="" for="myEvents">My Events</label>
 
   </div>
-  <events-post-cards-list @downvote="downvote" @upvote="upvote" :event_cards_list = "event_cards_list" />
+  <events-post-cards-list :event_cards_list = "event_cards_list" />
   <AddEventPlusButton style="position: fixed; bottom: 0px; right: 0px; height: 5rem; width: 5rem" />
 </template>
 
@@ -23,26 +27,43 @@
 import EventsPostCardsList from "../components/Event_Components/EventPostCardsList.vue";
 import EventsSearchBar from "../components/Event_Components/EventsSearchBar.vue";
 import AddEventPlusButton from "../components/Event_Components/AddEventPlusButton.vue";
-import Calendar from "../components/Event_Components/Calendar.vue";
 export default {
   name: "Events_Feed",
   components: {
     AddEventPlusButton,
     EventsPostCardsList,
-    EventsSearchBar,
-    Calendar
+    EventsSearchBar
   },
   data() {
     return {
       event_cards_list: [],
       lastSearch: "",
       type: "",
-      checkedMyEvents: null,
+      startDate: "",
+      endDate: "" ,
       event_types: [{name: 'Challenge'}, {name: 'Competition'}, {name: 'Meeting'}],
     }
   },
 
   methods: {
+
+    onChangeStart() {
+      if(this.endDate === null) {
+        this.endDate = this.startDate
+      } else {
+        if(this.startDate > this.endDate)
+        this.endDate = this.startDate
+      }
+    },
+
+    onChangeEnd() {
+      if(this.startDate === null) {
+        this.startDate = this.endDate
+      } else {
+        if(this.endDate < this.startDate)
+        this.startDate = this.endDate
+      }
+    },
 
     async fetchEventsPostCardsList (){
       const res = await fetch('api/event_cards_list')
